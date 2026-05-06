94de8a2ebc5e470486de53e949a0a54965c545e1
iosmitDev <mitesh.imply@gmail.com>
2026-05-06 16:43:21 +0530

    Initial project setup

---
+++ GitStepByStep/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitStepByStep
+//
+//  Created by MiteshKumar Patel on 06/05/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+//Adding to local and then remote
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
